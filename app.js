7fc01d90a8b29bb8d80f51adbed0186a9ecb7e95
mihal <vs6@mail.ru>
2017-10-03 20:32:03 +0300

    temporary fix

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/api', routesApi);
+//app.use('/api', routesApi);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
